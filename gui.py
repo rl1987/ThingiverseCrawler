bd0e3dcd8218bb876f10691c74ce71c37594eb76
rl1987 <rl1987@sdf.lonestar.org>
2019-11-05 12:34:57 +0200

    Implement basic input validation

--- gui.py
+++ gui.py
@@ -41,13 +41,31 @@ class CrawlerGUI():
         self.out_dir_entry.delete(0, tk.END)
         self.out_dir_entry.insert(0, out_dir)
 
+    def is_valid_url(self, url):
+        if not "thingiverse.com" in url:
+            return False
+
+        if not "collections" in url and not "explore" in url and not "search" in url:
+            return False
+
+        return True
+
     def start_button_pressed(self):
-        # TODO: validate
         out_dir = self.out_dir_entry.get()
+
+        if out_dir is None or len(out_dir) == 0:
+            messagebox.showerror('Error', 'Output directory path must be set')
+            return
 
         url = self.url_entry.get()
 
+        if not self.is_valid_url(url):
+            messagebox.showerror('Error', 'URL must be one of: 1) explore page (e.g. https://www.thingiverse.com/newest/), 2) search page (e.g. https://www.thingiverse.com/search?q=toy), 3) collection page (e.g. https://www.thingiverse.com/MakerTinkerSoldierSpy/collections/space-marines) without page parameter')
+            return
+
         url += "/page:{}"
+
+        print(url)
 
         crawler.crawl_things_internal(None, out_dir, url, None)
 
